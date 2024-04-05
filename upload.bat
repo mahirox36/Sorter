5dea7c29dfedad65f0fa8db30de99da28c0cf153
yazeed aloufi <ragontube1@gmail.com>
2024-04-05 06:40:21 +0300

    Updating Sorter

--- upload.bat
+++ upload.bat
@@ -2,5 +2,5 @@
 git add .
 git commit -m "Updating Sorter"
 git push origin
-pyinstaller --noconfirm --onefile --windowed --icon "G:/My Drive/TwinPixel/Icon/Normal/Icon.ico" --name "Sorter" --clean --hidden-import=_thread --hidden-import=win10toast "N:/python/Sorter/Sort.py"
+pyinstaller --noconfirm --onefile --windowed --icon "G:/My Drive/TwinPixel/Icon/Normal/Icon.ico" --name "Sorter" --clean --hidden-import=_thread --hidden-import=win10toast --hiden-import=pkg_resources "N:/python/Sorter/Sort.py"
 python make.py
