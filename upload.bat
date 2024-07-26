36ce4412e963dffaa4c3bf05f0d59a0cd74db704
mahirox36 <ragontube1@gmail.com>
2024-07-26 21:20:24 +0300

    Changes to be committed: 	modified:   upload.bat

--- upload.bat
+++ upload.bat
@@ -1,2 +1,6 @@
-pyinstaller --noconfirm --onefile --windowed --icon "Icon.ico" --name "Sorter" --clean "N:/python/Sorter/main.py" --add-data "N:/python/Sorter/Icon.ico;." --hidden-import=_thread --hidden-import=win10toast --hidden-import=pkg_resources --hidden-import=pystray --hidden-import=psutil
+@REM git pull origin
+git add .
+git commit -m "Updating Sorter"
+git push origin
+pyinstaller --noconfirm --onefile --windowed --icon "N:\python\Sorter\Icon.ico" --name "Sorter" --clean "N:/python/Sorter/main.py" --add-data "N:/python/Sorter/Icon.ico;." --hidden-import=_thread --hidden-import=win10toast --hidden-import=pkg_resources --hidden-import=pystray --hidden-import=psutil
 python make.py
